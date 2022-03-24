ced827e436cd0e88448d2f77938129866fa80829
hugochux <101514294+hugochux@users.noreply.github.com>
2022-03-24 14:39:42 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,11 @@
+# program to display calendar of August 2005
+
+# import tools (calendar module)
+import calendar
+
+mm = 8    # "mm = 8" means the 8th month of the year -August
+yy = 2005   # "yy = 2005" means the year
+
+#display the calendar
+
+print(calendar.month(yy, mm))
